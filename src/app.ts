924b532c1b88ab16050ac2738a8a4f18b2ba0ac6
Kayky Matos <kayky7277@gmail.com>
2023-01-09 16:06:40 -0300

    Iniciando implementação dos models e middlewares

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,16 @@ import router from "./router";
 import db from "../config/db"
 import mongoose from "mongoose";
 import Logger from "../config/logger";
+import morganMiddleware from "./middleware/morganMiddleware";
 
 const app = express();
 mongoose.set('strictQuery', false);
 const port = config.get<number>("port");
 //configura express com json
 app.use(express.json());
+
+app.use(morganMiddleware);
+
 //configura rota padrão
 app.use("/api/", router);
 
